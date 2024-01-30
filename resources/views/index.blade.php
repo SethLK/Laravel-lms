dfe3a750c3e7829a8191a5e4085a42209b5b4947
SethLK <sethlk2006@gmail.com>
2024-01-30 20:57:01 +0630

    Add new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@include("layouts.head")
+
+<h1>Hello, world!</h1>
+
+@include("layouts.foot")
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get("/login", [LoginController::class, "index"])->name("login");
+
