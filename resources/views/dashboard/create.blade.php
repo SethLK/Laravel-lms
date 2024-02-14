b390cb8a9d5f3a1fbccd0f94ca1787136b89e0ce
SethLK <sethlk2006@gmail.com>
2024-02-14 21:41:51 +0630

    Remove some

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -1,1 +1,11 @@
+
 <h1>Course Create</h1>
+<form action="" method="post" class="form-container">
+
+    <div class="form-group m-4">
+        <input type="text" placeholder="Course title" class="form-control-plaintext">
+    </div>
+
+    <button type="submit" class="btn btn-primary m-4">Confirm identity</button
+</form>
+
